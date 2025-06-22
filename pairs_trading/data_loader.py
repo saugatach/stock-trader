2bd04670672f665c12aff30776093699b227c977
saugatach <saugata.ch@gmail.com>
2025-06-21 20:50:55 -0700

    adding data dir using config file

--- pairs_trading/data_loader.py
+++ pairs_trading/data_loader.py
@@ -1,15 +1,25 @@
+# must run set_root to set the root
+import set_root
+import sys
+print(sys.path)
+import utils.load_config
+
 import pandas as pd
-import yfinance as yf
+from stockanalysis import getstockdata as gd
+from stockanalysis import helpers
 
-
-def fetch_ohlcv_data(tickers: list[str], start_date: str, end_date: str, output_file: str = "ohlcv_data.csv") -> pd.DataFrame:
+def fetch_ohlcv_data(tickers: list[str], start_date: str, end_date: str, output_file: str = "ohlcv_data.csv", verbose=True) -> pd.DataFrame:
     """Fetch adjusted close prices and save to CSV."""
-    try:
-        data = yf.download(tickers, start=start_date, end=end_date, progress=False)["Adj Close"]
-        data.dropna(how="all", inplace=True)
-        data.to_csv(output_file)
-    except Exception as e:
-        print(f"Error fetching data for tickers {tickers}: {e}")
+    settings = helpers.load_settings_stocks()
+    project_root = set_root.project_root
+    config = utils.file_utils.load_config(project_root=project_root, verbose=verbose)
+
+    stockobj = gd.GetStockData(tickers, settings=settings, verbose=verbose)
+    data = stockobj.getdata(start=start_date, end=end_date)
+    print(data)
+    data.dropna(how="all", inplace=True)
+    output_file = config['data'] + output_file
+    data.to_csv(output_file)
     return data
 
 
